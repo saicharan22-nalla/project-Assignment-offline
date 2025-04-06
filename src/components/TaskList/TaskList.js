668872c9296126b89d30ee80f034c18301530582
saicharan22-nalla <ssaicharan716@gmail.com>
2025-04-06 15:58:41 +0530

    Update TaskList.js

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,24 +1,3 @@
-/*import React from 'react';
-import TaskItem from './TaskItem';
-
-const TaskList = ({ tasks, deleteTask, toggleCompletion }) => {
-  return (
-    <ul>
-      {tasks.map(task => (
-        <TaskItem
-          key={task.id}
-          task={task}
-          deleteTask={deleteTask}
-          toggleCompletion={toggleCompletion}
-        />
-      ))}
-    </ul>
-  );
-};
-
-export default TaskList;*/
-
-
 import React from 'react';
 import TaskItem from './TaskItem';
 
